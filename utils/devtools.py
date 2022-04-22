947b848ddaf75ef7550cbbc0a9c20380959c1819
Liu Zihua <lukeliu2020@126.com>
2022-04-22 20:28:44 +0900

    add sceneflow dataset

---
+++ utils/devtools.py
@@ -0,0 +1,6 @@
+def print_tensor_shape(inputs):
+    if isinstance(inputs,list) or isinstance(inputs,tuple):
+        for value in inputs:
+            print(value.shape)
+    else:
+        print(inputs.shape)
