3b956ca1a5e931c6270b75b42e38a48c209a18cb
Liu Zihua <lukeliu2020@126.com>
2022-06-23 17:29:18 +0900

    add baseline

--- models/CrossAttentionCostVolume/baseline.py
+++ models/CrossAttentionCostVolume/baseline.py
@@ -87,6 +87,34 @@ class Baseline(nn.Module):
             nn.Conv2d(256,256,kernel_size=1,stride=1,padding=0,bias=False)
         )
         
+        
+        match_similarity = True
+        # 1/8 Scale Cost Volume
+        if self.cost_volume_type in ['correlation','concated']:
+            self.low_scale_cost_volume = CostVolume(max_disp=192//8,feature_similarity=self.cost_volume_type)
+
+        self.initial_conv = nn.Sequential(
+            ResBlock(24,24,3,1),
+            ResBlock(24,24,3,1)
+        )
+        
+        self.cost_aggregation = nn.Sequential(
+            ResBlock(24,24,3,1),
+            ResBlock(24,24,3,1),
+            ResBlock(24,24,3,1),
+            nn.Conv2d(in_channels=24,out_channels=24,kernel_size=1,stride=1,padding=0),
+            nn.ReLU(inplace=True)
+        )
+        
+        if self.upsample_type:
+            self.upsample_mask = ConvAffinityUpsample(input_channels=256,hidden_channels=128)
+
+
+
+        # 1/8 Scale Disparity Estimation
+        self.disp_estimation3 = DisparityEstimation(max_disp=192//8,match_similarity=match_similarity) 
+        
+        
         # Weight Initialization
         for m in self.modules():
             if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
@@ -105,41 +133,30 @@ class Baseline(nn.Module):
         conv1_l = self.conv1(img_left)          # 64 1/2
         conv2_l = self.conv2(conv1_l)           # 128 1/4
         conv3_l = self.conv3(conv2_l)           # 256 1/8
-        
+
         conv4_l = self.conv4(conv3_l)
         conv4_l_1 = self.conv4_1(conv4_l)   # 512 1/16
-        
         conv5_l = self.conv5(conv4_l_1)     # 512 1/32
         conv5_l_1 = self.conv5_1(conv5_l) 
-        
         conv6_l = self.conv6(conv5_l_1)       # 1024 1/64
         conv6_l_1 = self.conv6_1(conv6_l)     # 1024 1/64    
         conv7_l = self.up4(conv6_l_1)
         conv7_l_1 = F.relu(conv7_l+ self.redir4(conv5_l_1)) #512 1/32
-        
         conv8_l = self.up3(conv7_l_1)
         conv8_l_1 = F.relu(conv8_l+ self.redir3(conv4_l_1)) #512 1/16
-        
         conv9_l = self.up2(conv8_l_1)
         conv9_l_1 = F.relu(conv9_l+ self.redir2(conv3_l)) # 128 1/8
-
-        print(conv9_l_1.shape)
         
-    
         # Right Feature
         conv1_r = self.conv1(img_right)
         conv2_r = self.conv2(conv1_r)
         conv3_r = self.conv3(conv2_r)           # 1/8
-
         conv4_r = self.conv4(conv3_r)
         conv4_r_1 = self.conv4_1(conv4_r)   # 512 1/16
-        
         conv5_r = self.conv5(conv4_r_1)     # 512 1/32
         conv5_r_1 = self.conv5_1(conv5_r) 
-        
         conv6_r = self.conv6(conv5_r_1)       # 1024 1/64
         conv6_r_1 = self.conv6_1(conv6_r)     # 1024 1/64
-        
         
         conv7_r = self.up4(conv6_r_1)
         conv7_r_1 = F.relu(conv7_r+ self.redir4(conv5_r_1)) #512 1/32
@@ -151,14 +168,28 @@ class Baseline(nn.Module):
         conv9_r = self.up2(conv8_r_1)
         conv9_r_1 = F.relu(conv9_r+ self.redir2(conv3_r)) # 128 1/8
         
-    
-        print(conv9_r_1.shape)
+        # Build Cost Volume Here
+        low_scale_cost_volume3 = self.low_scale_cost_volume(conv9_l_1,conv9_r_1)
+        # Cost Volume Aggregation
+        cost_volume = self.initial_conv(low_scale_cost_volume3)
+        cost_volume = self.cost_aggregation(cost_volume)
+        
+        # Low Scale Disparity
+        low_scale_disp3 = self.disp_estimation3(cost_volume)
+        
+        assert low_scale_disp3.min()>=0
+
+        low_scale_disp3 = low_scale_disp3.unsqueeze(1)
+        
+        if self.upsample_type=='convex':
+            pr3_mask = self.upsample_mask(conv9_l_1)
+            pr0 = upsample_convex8(low_scale_disp3,pr3_mask)
+        elif self.upsample_type=='simple':
+            pr0 = upsample_simple8(low_scale_disp3)
         
         
-
-
-
-
+        return pr0
+        
 
 if __name__=="__main__":
     
@@ -169,5 +200,6 @@ if __name__=="__main__":
     baseline = Baseline(max_disp=192,cost_volume_type='correlation',
                         upsample_type='simple').cuda()
     
-    baseline(left,right)
+    out = baseline(left,right)
     
+    print(out.shape)
--- scripts/run.sh
+++ scripts/run.sh
@@ -35,4 +35,42 @@ python3 -W ignore train_low.py --cuda --loss $loss --lr $lr \
 }
 
 
-LowCNN
+
+Test(){
+cd ..
+loss=config/loss_config_disp.json
+outf_model=models_saved/Fuck
+logf=logs/Fuck
+datapath=/media/zliu/datagrid1/liu/sceneflow
+datathread=4
+lr=1e-3
+devices=0
+dataset=sceneflow
+trainlist=filenames/SceneFlow.list
+vallist=filenames/FlyingThings3D_release_TEST.list
+startR=0
+startE=0
+batchSize=4
+testbatch=8
+maxdisp=-1
+model=none
+save_logdir=experiments_logdir/Fuck
+model=Baseline
+pretrain=none
+
+python3 -W ignore train.py --cuda --loss $loss --lr $lr \
+               --outf $outf_model --logFile $logf \
+               --devices $devices --batch_size $batchSize \
+               --dataset $dataset --trainlist $trainlist --vallist $vallist \
+               --startRound $startR --startEpoch $startE \
+               --model $model \
+               --maxdisp $maxdisp \
+               --datapath $datapath \
+               --manualSeed 1024 --test_batch $testbatch \
+               --save_logdir $save_logdir \
+               --pretrain $pretrain  
+}
+
+
+# LowCNN
+Test
